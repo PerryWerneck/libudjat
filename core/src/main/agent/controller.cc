83dd9d5c7984ebee08c4d84a30b40bb3082a7ed9
Perry Werneck <perry.werneck@gmail.com>
2021-03-10 22:17:57 -0300

    Refactoring agent controller as worker.

--- core/src/main/agent/controller.cc
+++ core/src/main/agent/controller.cc
@@ -9,6 +9,7 @@
 
  #include "private.h"
  #include <udjat/tools/threadpool.h>
+ #include <udjat/worker.h>
 
  using namespace std;
 
@@ -16,18 +17,50 @@
 
 namespace Udjat {
 
-	static std::shared_ptr<Abstract::Agent> root_agent;
+	class AgentController : public Worker {
+	private:
+		std::shared_ptr<Abstract::Agent> root;
+
+	public:
+
+		AgentController() : Worker(I_("agent")) {
+			this->active = true;
+		}
+
+		void set(std::shared_ptr<Abstract::Agent> root) {
+			this->root = root;
+			this->active = this->root.get();
+		}
+
+		std::shared_ptr<Abstract::Agent> get() {
+
+			if(this->root)
+				return this->root;
+
+			throw runtime_error("Agent subsystem is inactive");
+
+		}
+
+		static AgentController & getInstance() {
+			static AgentController controller;
+			return controller;
+		}
+
+		void work(const char *path, const Request &request, Response &response) {
+
+			throw runtime_error("Not implemented");
+
+		}
+
+	};
+
 
 	void set_root_agent(std::shared_ptr<Abstract::Agent> agent) {
-		root_agent = agent;
+		AgentController::getInstance().set(agent);
 	}
 
 	std::shared_ptr<Abstract::Agent> get_root_agent() {
-
-		if(!root_agent)
-			throw runtime_error("Agent controller is non existent or inactive");
-
-		return root_agent;
+		return AgentController::getInstance().get();
 	}
 
 	std::shared_ptr<Abstract::Agent> find_agent(const char *path) {
@@ -35,9 +68,9 @@ namespace Udjat {
 		auto root = get_root_agent();
 
 		if(path && *path)
-			return root_agent->find(path);
+			return root->find(path);
 
-		return root_agent;
+		return root;
 
 	}
 
