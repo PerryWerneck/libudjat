aacb1664bdbe9cad2ab2eaabbc7d85f88ee5f3db
Perry Werneck <perry.werneck@gmail.com>
2021-06-07 22:59:35 -0300

    Detecting virtual machine using systemd.

--- src/main/agent/root.cc
+++ src/main/agent/root.cc
@@ -24,6 +24,10 @@
  #include <cstring>
  #include <unistd.h>
 
+ #ifdef HAVE_SYSTEMD
+	#include <systemd/sd-bus.h>
+ #endif // HAVE_SYSTEMD
+
  namespace Udjat {
 
 	std::shared_ptr<Abstract::Agent> getDefaultRootAgent() {
@@ -68,34 +72,120 @@
 				}
 
 				//
-				// Get machine information
+				// Detect virtualization.
 				//
-				try {
+				string virtualization;
 
-					static const char *ids[] = {
-						"dmi.1.0.1",
-						"dmi.1.0.2"
-					};
+#ifdef HAVE_SYSTEMD
+				{
+					// Detected virtualization with systemd.
 
-					string id;
+					// Reference: (https://www.freedesktop.org/software/systemd/man/org.freedesktop.systemd1.html)
+					sd_bus * bus = NULL;
+					if(sd_bus_open_system(&bus) < 0) {
 
-					for(size_t ix = 0; ix < (sizeof(ids)/sizeof(ids[0])); ix++) {
+						clog << getName() << "\tCant open system bus" << endl;
 
-						if(!id.empty()) {
-							id += " ";
+					} else {
+
+						/*
+							dbus-send \
+								--session \
+								--dest=org.freedesktop.systemd1 \
+								--print-reply \
+								"/org/freedesktop/systemd1" \
+								"org.freedesktop.DBus.Properties.Get" \
+								string:"org.freedesktop.systemd1.Manager" \
+								string:"Virtualization"
+						*/
+
+						sd_bus_error error = SD_BUS_ERROR_NULL;
+						sd_bus_message *response = NULL;
+
+						int rc = sd_bus_call_method(
+								bus,                   					// On the System Bus
+                                "org.freedesktop.systemd1",				// Service to contact
+                                "/org/freedesktop/systemd1", 			// Object path
+                                "org.freedesktop.DBus.Properties",		// Interface name
+                                "Get",									// Method to be called
+                                &error,									// object to return error
+                                &response,								// Response message on success
+                                "ss",
+                                "org.freedesktop.systemd1.Manager",
+                                "Virtualization"
+						);
+
+						if(rc < 0) {
+
+							cerr << getName() << "\t" << error.message << endl;
+							sd_bus_error_free(&error);
+
+						} else if(response) {
+
+							char *text = NULL;
+
+							// It's a variant
+
+							if(sd_bus_message_read(response, "v", "s", &text) < 0) {
+								cerr << getName() << "\tCan't parse systemd virtualization response" << endl;
+							} else if(text && *text) {
+								cout << getName() << "\t Detected '" << text << "' virtualization" << endl;
+								virtualization = text;
+							}
+
+							sd_bus_message_unref(response);
+
 						}
 
-						id += Factory::get(ids[ix])->to_string();
+						sd_bus_flush_close_unref(bus);
 
 					}
 
-					if(!id.empty()) {
-						this->summary = Quark(id).c_str();
+				}
+#endif // HAVE_SYSTEMD
+
+				//
+				// Get machine information
+				//
+				if(virtualization.empty()) {
+
+					try {
+
+						static const char *ids[] = {
+							"dmi.1.0.1",
+							"dmi.1.0.2"
+						};
+
+						string id;
+
+						for(size_t ix = 0; ix < (sizeof(ids)/sizeof(ids[0])); ix++) {
+
+							if(!id.empty()) {
+								id += " ";
+							}
+
+							id += Factory::get(ids[ix])->to_string();
+
+						}
+
+						if(!id.empty()) {
+							this->summary = Quark(id).c_str();
+						}
+
+					} catch(const std::exception &e) {
+
+						warning("Error '{}' getting DMI information",e.what());
+
+					}
+				} else {
+
+					string text{"{} virtual machine"};
+					size_t pos = text.find("{}");
+					if(pos != string::npos) {
+						text.replace(pos,2,virtualization);
 					}
 
-				} catch(const std::exception &e) {
-
-					warning("Error '{}' getting DMI information",e.what());
+					this->summary = Quark(text).c_str();
 
 				}
 
