82471e19bcc807698e3ba2df3f55d2a9433cdfde
Perry Werneck <perry.werneck@gmail.com>
2021-12-21 23:40:39 -0300

    Fixing warning.

--- src/tools/os/windows/configvalue.cc
+++ src/tools/os/windows/configvalue.cc
@@ -1,10 +1,11 @@
-/**
- *
- * Copyright (C) <2017> <Perry Werneck>
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+/*
+ * Copyright (C) 2021 Perry Werneck <perry.werneck@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,19 +13,21 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
+/** 
  * @file
  *
- * @brief Implements linux configuration file.
+ * @brief Implements windows configuration file abstraction.
  *
  * @author Perry Werneck <perry.werneck@gmail.com>
  *
  */
 
  #include <config.h>
- #include <windows.h>
+ #include <udjat/defs.h>
  #include <udjat/tools/configuration.h>
  #include <udjat/tools/application.h>
  #include <udjat/tools/quark.h>
