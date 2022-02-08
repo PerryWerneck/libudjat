6fa690f351dc35d0692d0dc816312f41dfa2ea92
Perry Werneck <perry.werneck@gmail.com>
2022-02-08 11:50:45 -0300

    Fixing xml parser.

--- src/tools/xml.cc
+++ src/tools/xml.cc
@@ -22,6 +22,7 @@
  #include <udjat/tools/xml.h>
  #include <udjat/tools/logger.h>
  #include <udjat/tools/expander.h>
+ #include <udjat/tools/object.h>
  #include <iostream>
  #include <cstdarg>
 
@@ -107,13 +108,21 @@
 
 		expand(text,[node](const char *key, string &value){
 
-			Attribute attribute(node,key,key);
+			pugi::xml_attribute attribute;
+
+			attribute = Object::getAttribute(node,key);
 			if(attribute) {
 				value = attribute.as_string();
 				return true;
 			}
 
-			// Not expanded, return fixed value.
+			attribute = Object::getAttribute(node,key,false);
+			if(attribute) {
+				value = attribute.as_string();
+				return true;
+			}
+
+			// Not expanded
 			return false;
 
 		});
