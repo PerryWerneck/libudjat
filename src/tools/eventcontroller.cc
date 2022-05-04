371306a6d2551edfdf718aca26352fd534147480
Perry Werneck <perry.werneck@gmail.com>
2022-05-04 14:17:44 -0300

    Add missing sources.

---
+++ src/tools/eventcontroller.cc
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+/*
+ * Copyright (C) 2021 Perry Werneck <perry.werneck@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ #include <private/event.h>
+ #include <iostream>
+
+ using namespace std;
+
+ namespace Udjat {
+
+	mutex guard;
+
+	Event::Controller::Controller() {
+		cout << "event\tStarting controller " << hex << this << dec << endl;
+	}
+
+	Event::Controller::~Controller() {
+		cout << "event\tStopping controller " << hex << this << dec << endl;
+	}
+
+	Event::Controller & Event::Controller::getInstance() {
+		lock_guard<mutex> lock(guard);
+		static Controller instance;
+		return instance;
+	}
+
+
+ }
+
