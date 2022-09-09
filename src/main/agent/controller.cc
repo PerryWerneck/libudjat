f22aff385406a90b223d342ae431c0cbb59501a3
Perry Werneck <perry.werneck@gmail.com>
2022-09-09 13:38:10 -0300

    Waiting for background tasks after stopping agents.

--- src/main/agent/controller.cc
+++ src/main/agent/controller.cc
@@ -1,7 +1,26 @@
-/**
- * @file src/core/agent/controller.cc
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+/*
+ * Copyright (C) 2021 Perry Werneck <perry.werneck@gmail.com>
  *
- * @brief Implements the root agent.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file src/main/agent/controller.cc
+ *
+ * @brief Implements the agent controller.
  *
  * @author perry.werneck@gmail.com
  *
@@ -183,6 +202,11 @@ namespace Udjat {
 			}
 
 			root.reset();
+
+#ifdef DEBUG
+			cout << "*** Waiting for tasks" << endl;
+#endif // DEBUG
+			ThreadPool::getInstance().wait();
 
 		}
 
