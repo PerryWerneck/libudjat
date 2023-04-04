b55e1291d186a343b13f07fe22adeee123a98530
Perry Werneck <perry.werneck@gmail.com>
2023-04-03 23:00:32 -0300

    Update ci-build.sh

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -21,13 +21,13 @@ cd $(dirname $(dirname $(readlink -f ${0})))
 #
 # Install pre-reqs
 #
-pacman -U --noconfirm *.pkg.tar.zst > $LOGFILE 2>&1 || die "pacman failure"
+pacman -U --noconfirm *.pkg.tar.zst || die "pacman failure"
 
 #
 # Build
 #
-dos2unix PKGBUILD.mingw
-makepkg BUILDDIR=/tmp/pkg -p PKGBUILD.mingw > $LOGFILE 2>&1 || die "makepkg failure"
+dos2unix PKGBUILD.mingw  || die "dos2unix failure"
+makepkg BUILDDIR=/tmp/pkg -p PKGBUILD.mingw || die "makepkg failure"
 
 echo "Build complete"
 
