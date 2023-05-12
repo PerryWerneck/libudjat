ae5d06334efcaf4607acbdf5a65988b45fc82c59
Perry Werneck <perry.werneck@gmail.com>
2023-05-12 15:40:02 -0300

    Debugging win32 action.

--- src/include/udjat/tools/timestamp.h
+++ src/include/udjat/tools/timestamp.h
@@ -17,122 +17,121 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef TIMESTAMP_H_INCLUDED
+ #pragma once
 
-	#define TIMESTAMP_H_INCLUDED
+#include <udjat/defs.h>
+#include <ctime>
+#include <string>
+#include <cstdint>
 
-	#include <udjat/defs.h>
-	#include <ctime>
-	#include <string>
+ namespace Udjat {
 
-	namespace Udjat {
+	#define TIMESTAMP_FORMAT_JSON "%Y/%m/%d %H:%M:%S"
 
-		#define TIMESTAMP_FORMAT_JSON "%Y/%m/%d %H:%M:%S"
+	/// @brief A time value (in seconds)
+	class UDJAT_API TimeStamp {
+	protected:
+		time_t value;
 
-		/// @brief A time value (in seconds)
-		class UDJAT_API TimeStamp {
-		protected:
-			time_t value;
+	public:
 
-		public:
+		constexpr TimeStamp(time_t t = time(nullptr)) : value(t) { }
 
-			constexpr TimeStamp(time_t t = time(nullptr)) : value(t) { }
+		explicit TimeStamp(const char *time, const char *format = nullptr);
 
-			explicit TimeStamp(const char *time, const char *format = nullptr);
+		std::string to_string(const char *format = "%x %X") const noexcept;
 
-			std::string to_string(const char *format = "%x %X") const noexcept;
+		/// @brief Format as json date/time
+		inline std::string to_json() const noexcept {
+			return to_string(TIMESTAMP_FORMAT_JSON);
+		}
 
-			/// @brief Format as json date/time
-			inline std::string to_json() const noexcept {
-				return to_string(TIMESTAMP_FORMAT_JSON);
-			}
+		std::string to_verbose_string() const noexcept;
 
-			std::string to_verbose_string() const noexcept;
+		inline std::string to_string(const std::string &format) const noexcept {
+			return to_string(format.c_str());
+		}
 
-			inline std::string to_string(const std::string &format) const noexcept {
-				return to_string(format.c_str());
-			}
+		inline operator bool() const noexcept {
+			return value != 0;
+		}
 
-			inline operator bool() const noexcept {
-				return value != 0;
-			}
+		/// @brief Set a time delay.
+		/// @param seconds	Seconds after current timestamp to set.
+		TimeStamp & reset(const uint32_t seconds) noexcept {
+			value = ::time(nullptr) + seconds;
+			return *this;
+		}
 
-			/// @brief Set a time delay.
-			/// @param seconds	Seconds after current timestamp to set.
-			TimeStamp & reset(const uint32_t seconds) noexcept {
-				value = ::time(nullptr) + seconds;
-				return *this;
-			}
+		TimeStamp & reset(const char *time, const char *format = nullptr) {
+			value = ::time(nullptr) + parse(time,format);
+			return *this;
+		}
 
-			TimeStamp & reset(const char *time, const char *format = nullptr) {
-				value = ::time(nullptr) + parse(time,format);
-				return *this;
-			}
+		static time_t parse(const char *time, const char *format = nullptr);
 
-			static time_t parse(const char *time, const char *format = nullptr);
+		TimeStamp & set(const char *time, const char *format = nullptr);
 
-			TimeStamp & set(const char *time, const char *format = nullptr);
+		TimeStamp & operator=(const time_t t) noexcept {
+			value = t;
+			return *this;
+		}
 
-			TimeStamp & operator=(const time_t t) noexcept {
-				value = t;
-				return *this;
-			}
+		inline TimeStamp & operator=(const char *time) {
+			return set(time);
+		}
 
-			inline TimeStamp & operator=(const char *time) {
-				return set(time);
-			}
+		inline operator time_t() const noexcept {
+			return this->value;
+		}
 
-			inline operator time_t() const noexcept {
-				return this->value;
-			}
+		inline time_t as_value() const noexcept {
+			return this->value;
+		}
 
-			inline time_t as_value() const noexcept {
-				return this->value;
-			}
+		bool operator==(time_t value) const noexcept {
+			return this->value == value;
+		}
 
-			bool operator==(time_t value) const noexcept {
-				return this->value == value;
-			}
+		bool operator<(time_t value) const noexcept {
+			return this->value < value;
+		}
 
-			bool operator<(time_t value) const noexcept {
-				return this->value < value;
-			}
+		bool operator>(time_t value) const noexcept {
+			return this->value > value;
+		}
 
-			bool operator>(time_t value) const noexcept {
-				return this->value > value;
-			}
+		TimeStamp & operator-(time_t value) noexcept {
+			this->value -= value;
+			return *this;
+		}
 
-			TimeStamp & operator-(time_t value) noexcept {
-				this->value -= value;
-				return *this;
-			}
+		time_t operator+(time_t value) noexcept {
+			this->value -= value;
+			return *this;
+		}
 
-			time_t operator+(time_t value) noexcept {
-				this->value -= value;
-				return *this;
-			}
+		void operator ++() {
+			++value;
+		}
 
-			void operator ++() {
-				++value;
-			}
+		void operator --() {
+			++value;
+		}
 
-			void operator --() {
-				++value;
-			}
+		void operator ++ (int) {
+			value++;
+		}
 
-			void operator ++ (int) {
-				value++;
-			}
+		void operator -- (int) {
+			value--;
+		}
 
-			void operator -- (int) {
-				value--;
-			}
+	};
 
-		};
+ }
 
-	}
-
-namespace std {
+ namespace std {
 
 	inline string to_string(const Udjat::TimeStamp &time) {
 		return time.to_string();
@@ -142,7 +141,6 @@ namespace std {
 		return os << time.to_string();
 	}
 
-}
+ }
 
 
-#endif // TIMESTAMP_H_INCLUDED
