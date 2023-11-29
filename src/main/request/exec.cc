2fd75ce4581c6d2929ce20a66c6336d437e5388c
Perry Werneck <perry.werneck@gmail.com>
2023-11-29 01:15:54 -0300

    Refactoring request class.

---
+++ src/main/request/exec.cc
@@ -0,0 +1,95 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+/*
+ * Copyright (C) 2023 Perry Werneck <perry.werneck@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ /**
+  * @brief Implements Request::exec.
+  */
+
+ #include <config.h>
+ #include <udjat/tools/logger.h>
+ #include <udjat/tools/intl.h>
+ #include <udjat/tools/request.h>
+ #include <udjat/tools/worker.h>
+ #include <stdexcept>
+
+ using namespace std;
+
+ namespace Udjat {
+
+	void Request::exec(Response &response) {
+
+		const Worker *selected_worker = nullptr;
+
+		debug("Searching worker for '",path.c_str(),"'");
+
+		if(!Worker::for_each([this,&selected_worker](const Worker &worker){
+
+			const char *path{worker.check_path(this->path.c_str())};
+			if(!path) {
+				return false;
+			}
+
+			debug("Worker '",worker.c_str(),"' selected path '",path,"'");
+
+			selected_worker = &worker;
+			this->path = path;
+
+			return true;
+
+		})) {
+
+			throw system_error(ENOENT,system_category(),Logger::String{"Cant handle '",this->path,"'"});
+
+		}
+
+		selected_worker->work(*this,response);
+
+	}
+
+	void Request::exec(Report &response) {
+
+		const Worker *selected_worker = nullptr;
+
+		debug("Searching worker for '",path.c_str(),"'");
+
+		if(!Worker::for_each([this,&selected_worker](const Worker &worker){
+
+			const char *path{worker.check_path(this->path.c_str())};
+			if(!path) {
+				return false;
+			}
+
+			debug("Worker '",worker.c_str(),"' selected path '",path,"'");
+
+			selected_worker = &worker;
+			this->path = path;
+
+			return true;
+
+		})) {
+
+			throw system_error(ENOENT,system_category(),Logger::String{"Cant handle '",this->path,"'"});
+
+		}
+
+		selected_worker->work(*this,response);
+
+	}
+
+ }
