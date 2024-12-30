f907c039df68f166f2d94c3c36995366288ca4db
Perry Werneck <perry.werneck@gmail.com>
2024-12-30 11:19:41 -0300

    Adding convenience method to conver integer to hex strings.

--- src/include/udjat/tools/logger.h
+++ src/include/udjat/tools/logger.h
@@ -31,6 +31,15 @@
 
  namespace Udjat {
 
+	template <typename I> 
+	inline std::string n2hexstr(I w, size_t hex_len = sizeof(I)<<1) {
+		static const char* digits = "0123456789ABCDEF";
+		std::string rc(hex_len,'0');
+		for (size_t i=0, j=(hex_len-1)*4 ; i<hex_len; ++i,j-=4)
+			rc[i] = digits[(w>>j) & 0x0f];
+		return rc;
+	}
+
 	/// @brief Manage log files.
 	/// Reference: https://www.youtube.com/watch?v=IdM0Z2a4fjU
 	namespace Logger {
