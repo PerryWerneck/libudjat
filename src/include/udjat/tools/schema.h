04abc1895276a5fcca3cd37681bfe35c210a87e8
Perry Werneck <perry.werneck@gmail.com>
2026-07-07 00:01:18 -0300

    Adding a Schema class declarations, just a reminder for now.

---
+++ src/include/udjat/tools/schema.h
@@ -0,0 +1,79 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+/*
+ * Copyright (C) 2026 Perry Werneck <perry.werneck@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ #pragma once
+
+ #include <udjat/defs.h>
+ #include <udjat/tools/value.h>
+ #include <vector>
+
+ namespace Udjat {
+
+	/// @brief Encapsulates the structural layout and validation rules used to parse, verify, and serialize structured data formats.
+	class UDJAT_API Schema {
+	public:
+
+		class UDJAT_API Item {
+		protected:
+			const char *name;					///< @brief The item name.
+			Value::Type type;					///< @brief The value type.
+			const char *description = nullptr;	///< @brief The item description.
+
+		public:
+			constexpr Item(const char *n, Value::Type t)
+				: name{n}, type{t} { }
+
+			constexpr Item(const char *n, Value::Type t, const char *d)
+				: name{n}, type{t}, description{d} { }
+
+		};
+
+		Schema() {
+		}
+
+		template<typename... Targs>
+		Schema(Targs... Fargs) {
+			append(Fargs...);
+		}
+
+		inline void append(const Item &item) {
+			itens.push_back(item);
+		}
+
+		template<typename... Targs>
+		inline void append(const Item &item, Targs... Fargs) {
+			append(item);
+			append(Fargs...);
+		}
+
+		inline auto begin() const noexcept {
+			return itens.begin();
+		}
+
+		inline auto end() const noexcept {
+			return itens.end();
+		}
+
+	private:
+		std::vector<Item> itens;
+
+	};
+
+ }
+
