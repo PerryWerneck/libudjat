ebfe22a26c9df2965d55957173334835cf91dad1
Perry Werneck <perry.werneck@gmail.com>
2026-07-08 18:12:42 -0300

    Removing log debug messages.

--- src/library/tools/logger/level.cc
+++ src/library/tools/logger/level.cc
@@ -75,21 +75,10 @@
 			level = LOGGER_MAX_VERBOSITY;
 		}
 
-#ifdef DEBUG
-		cout << "Selected level:" << level << endl;
-#endif // DEBUG
-
 		enabled_levels = Logger::Level::None;
 		for(int ix=0; ix < level; ix++) {
-#ifdef DEBUG
-			cout << "Enabling log level '" << levels[ix].name << "' value=" << levels[ix].level << endl;
-#endif // DEBUG
 			enabled_levels = (Logger::Level) (enabled_levels|levels[ix].level);
 		}
-
-#ifdef DEBUG
-		cout <<  "Current log level is " << enabled_levels << endl;
-#endif // DEBUG
 
 	}
 
